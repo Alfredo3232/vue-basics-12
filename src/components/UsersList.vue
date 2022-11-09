85a273be94629dffbbcf761f66fcd883f5d33042
arojas32 <61888719+arojas32@users.noreply.github.com>
2022-11-08 20:50:54 -0600

    add components and try to implement animations with vue-router

--- src/components/UsersList.vue
+++ src/components/UsersList.vue
@@ -1,7 +1,7 @@
 <template>
-    <ul>
+    <transition-group tag="ul" name="user-list">
         <li v-for="user in users" :key="user" @click="removeUser(user)">{{ user }}</li>
-    </ul>
+    </transition-group>
     <div>
         <input type="text" ref="userNameInput" />
         <button @click="addUser">Add User</button>
@@ -38,5 +38,35 @@ li {
     border: 1px solid #ccc;
     padding: 1rem;
     text-align: center;
+}
+
+.user-list-enter-from {
+    opacity: 0;
+    transform: translateX(-30px);
+}
+
+.user-list-enter-active {
+    transition: all 1s ease-out;
+}
+
+.user-list-enter-to,
+.user-list-leave-from {
+
+    opacity: 1;
+    transform: translateX(0);
+}
+
+.user-list-leave-active {
+    transition: all 1s ease-in;
+    position: absolute;
+}
+
+.user-list-leave-to {
+    opacity: 0;
+    transform: translateX(30px);
+}
+
+.user-list-move {
+    transition: transform 0.8s ease;
 }
 </style>
